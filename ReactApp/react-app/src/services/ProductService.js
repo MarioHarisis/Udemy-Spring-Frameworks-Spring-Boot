7245f50f0aba2b8a352c4ddf827c5a8f9551eabd
Mario Harisis Garrido <147819942+MarioHarisis@users.noreply.github.com>
2025-03-19 18:10:29 +0100

    ID added

--- ReactApp/react-app/src/services/ProductService.js
+++ ReactApp/react-app/src/services/ProductService.js
@@ -1,11 +1,13 @@
 // creación lista de productos
 const initProducts = [
   {
+    id: 1,
     name: "Monitor Samsung",
     price: 500,
     description: "El momnitor es genial",
   },
   {
+    id: 2,
     name: "iPhone 15",
     price: 1200,
     description: "Telefono increible",
