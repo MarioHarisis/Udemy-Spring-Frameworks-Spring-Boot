878041623496f39886c094c37b6a33a6e23dfba7
Mario Harisis Garrido <147819942+MarioHarisis@users.noreply.github.com>
2025-03-25 20:32:42 +0100

    Delete springboot-aop/src/main/springboot_aop directory

--- springboot-aop/src/main/springboot_aop/services/GreetingService.java
+++
@@ -1,7 +0,0 @@
-package com.sprinboot.app.interceptor.springboot_aop.services;
-
-public interface GreetingService {
-    String sayHello(String person, String phrase);
-
-    String sayHelloError(String person, String phrase);
-}
